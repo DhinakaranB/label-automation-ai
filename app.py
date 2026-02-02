dfb53a15ae4da5de7247de2040500d8bd16ffc99
Dhinakaran B <dhinakaran.b@example.com>
2026-02-02 15:10:38 +0530

    Add deployment files

--- app.py
+++ app.py
@@ -245,4 +245,5 @@ def convert_to_svg(pdf_filename, svg_filename):
         return None
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
